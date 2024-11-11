8064a032c2a6826e0c9056a2f9e3206d437bd9ee
HwangSgHn <dolphin33@jbnu.ac.kr>
2024-11-11 11:18:52 +0900

    feat: 구매한 만큼 수량을 감소하는 기능

--- src/main/java/store/Product.java
+++ src/main/java/store/Product.java
@@ -1,6 +1,9 @@
 package store;
 
+import java.text.DecimalFormat;
+
 public class Product {
+    private static final DecimalFormat formatter = new DecimalFormat("###,###");
     private final String name;
     private final int price;
     private int quantity;
@@ -26,5 +29,18 @@ public class Product {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public void decreaseQuantity(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    @Override
+    public String toString() {
+        String quantityStr = quantity + "개";
+        if ("0개".equals(quantityStr)) {
+            quantityStr = "재고 없음";
+        }
+        return name + " " + formatter.format(price) + "원 " + quantityStr + " " + promotionName;
     }
 }
